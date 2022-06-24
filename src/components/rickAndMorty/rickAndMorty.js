f03f9a0f63b6a5ea390633d521fcf9d64234145d
OlehMartyniak <97449514+OlehMartyniak@users.noreply.github.com>
2022-06-24 23:28:06 +0300

    React main

---
+++ src/components/rickAndMorty/rickAndMorty.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const RickAndMorty = (props) => {
+    const {id,name,status,species,gender,image} = props;
+    return (
+        <div>
+            <h5>{id}</h5>
+            <h4>{name}</h4>
+            <p>{status}</p>
+            <p>{species}</p>
+            <p>{gender}</p>
+            <img src={image} alt={name}/>
+        </div>
+    );
+};
+
+export default RickAndMorty;
